7e3a6547baae4ff527edf43e1196a52d2241331f
valevurro <vale.v.00@outlook.it>
2025-11-05 20:00:56 +0100

    fix: correct function name from logTime to longTime

--- middleware/longTime.js
+++ middleware/longTime.js
@@ -1,4 +1,4 @@
-function logTime(req, res, next) {
+function longTime(req, res, next) {
 
     const now = new Date().toLocaleString();
     console.log('[${now}] Got a new request for url: ${req.url} - method $(req.method');
